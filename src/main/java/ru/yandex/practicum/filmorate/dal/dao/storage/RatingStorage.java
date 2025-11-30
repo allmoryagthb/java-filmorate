c3c8a13d804a3b142eaaa11b2e6a96400c977f7e
Alexey Morozov <allmorya@yandex.ru>
2025-11-30 15:46:16 +0300

    wip

--- src/main/java/ru/yandex/practicum/filmorate/dal/dao/storage/RatingStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/dao/storage/RatingStorage.java
@@ -5,7 +5,7 @@ import ru.yandex.practicum.filmorate.model.Rating;
 import java.util.Collection;
 
 public interface RatingStorage {
-
     Collection<Rating> getAllRatings();
+
     Rating getRatingById(Long id);
 }
